d8ee821e2a82a41966854bd67419d5dff4e23a8d
lucifer1708 <hello@sumitdhiman.in>
2023-12-27 17:07:31 +0530

    fixed linting

--- fetch_data.py
+++ fetch_data.py
@@ -1,14 +1,17 @@
+# import html
+
+import html2text
 import requests
-import json
 
 url = "https://indeed12.p.rapidapi.com/job/02eb3a9f080f10e7"
 
 headers = {
-	"X-RapidAPI-Key": "6e90ef82cemshd8c60a80c4947ffp15d5fejsnadb81d054fe6",
-	"X-RapidAPI-Host": "indeed12.p.rapidapi.com"
+    "X-RapidAPI-Key": "6e90ef82cemshd8c60a80c4947ffp15d5fejsnadb81d054fe6",
+    "X-RapidAPI-Host": "indeed12.p.rapidapi.com",
 }
 
 response = requests.get(url, headers=headers)
-jobs= response.json()
-
-for job in jobs.get():
+jobs = response.json()
+h = html2text.HTML2Text()
+h.ignore_links = True
+print(h.handle(jobs["description"]))
