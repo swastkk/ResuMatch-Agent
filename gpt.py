8927cf9e25fb4868a523ad82a8e46c43932872db
lucifer1708 <hello@sumitdhiman.in>
2023-12-27 16:39:25 +0530

    install pip

--- gpt.py
+++ gpt.py
@@ -1,28 +1,39 @@
-import requests
 import os
-from pyresparser import ResumeParser
+
+import requests
 from dotenv import load_dotenv
+from pyresparser import ResumeParser
+
 load_dotenv()
 
-API_KEY= os.getenv('OPEN_API_KEY')
+API_KEY = os.getenv("OPEN_API_KEY")
 # Replace with your actual API endpoint and key
-chatgpt_api_endpoint = "https://api.openai.com/v1/engines/chatgpt-model-name/completions"
-headers = {"Authorization": "", API_KEY}
+chatgpt_api_endpoint = (
+    "https://api.openai.com/v1/engines/chatgpt-model-name/completions"
+)
+headers = {"Authorization": f"{API_KEY}"}
+
 
 def parse_resume(resume_path):
     # Parse the resume
     data = ResumeParser(resume_path).get_extracted_data()
     return data
 
+
 def score_resume(resume_data, job_description):
     # Format the data for the ChatGPT API
     prompt = f"Please evaluate the following resume based on the job description provided: \nResume: {resume_data}\nJob Description: {job_description}\nScore:"
-    
-    response = requests.post(chatgpt_api_endpoint, headers=headers, json={"prompt": prompt, "max_tokens": 150})
+
+    response = requests.post(
+        chatgpt_api_endpoint,
+        headers=headers,
+        json={"prompt": prompt, "max_tokens": 150},
+    )
     if response.status_code == 200:
-        return response.json().get('choices')[0].get('text').strip()
+        return response.json().get("choices")[0].get("text").strip()
     else:
         return "Error in ChatGPT API response"
+
 
 # Path to your resume file
 resume_path = "path/to/resume.pdf"
